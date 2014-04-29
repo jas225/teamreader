ca925fb77b2165fdd8e940e77d5cf71f019db8af
jas225 <james.shook@gmail.com>
2014-04-28 21:07:28 -0400

    first files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teamreader::Application.config.secret_key_base = 'a57c3af563805275a9fcc2225a8d3cda0af3206deafab29214e897011584c2b64117ccd8fccef95a1f1031b612d76bda407a2a983483d235885abf0825bf69a1'
