ae0a1334afdaa059301a0ae709366dff4e01107f
jas225 <james.shook@gmail.com>
2014-05-04 17:52:13 -0400

    scaffold of links

---
+++ app/views/links/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @link, :id, :url, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Teamreader::Application.routes.draw do
+  resources :links
+
   devise_for :users
   root "pages#home"
 
