8a1640615a2db3631da04f83cea4ddaad091c196
jas225 <james.shook@gmail.com>
2014-05-04 18:51:34 -0400

    added associate between links and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504215158) do
+ActiveRecord::Schema.define(version: 20140504224736) do
 
   create_table "links", force: true do |t|
     t.string   "url"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
